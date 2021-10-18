8688bb828f58e7250163b0b5bd47b22b3a4cd47b
bhuvan2533 <bhuvn2582@gmail.com>
2021-10-18 13:34:51 +0530

    Replace high quality videos and fixed logo render problem

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -14,6 +14,24 @@ import Rain from "../videos/Rain.mp4";
 import Smoke from "../videos/Smoke.mp4";
 import Thunderstorm from "../videos/Thunderstorm.mp4";
 
+import c01d from "../Images/01d.png";
+import c01n from "../Images/01n.png";
+import c02d from "../Images/02d.png";
+import c02n from "../Images/02n.png";
+import c03d from "../Images/03d.png";
+import c03n from "../Images/03n.png";
+import c04d from "../Images/04d.png";
+import c04n from "../Images/04n.png";
+import c09d from "../Images/09d.png";
+import c09n from "../Images/09n.png";
+import c10d from "../Images/10d.png";
+import c10n from "../Images/10d.png";
+import c11d from "../Images/11d.png";
+import c11n from "../Images/11n.png";
+import c50d from "../Images/50d.png";
+import c50n from "../Images/50n.png";
+import unknown from "../Images/unknown.png";
+
 import humidity from "../Images/humidity.svg";
 import sunrise from "../Images/sunrise.svg";
 import pressure from "../Images/pressure.svg";
@@ -31,38 +49,23 @@ export const weatherBackground = {
 };
 // importing icons
 export const weatherIcons = {
-  "01d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/c238cf321ca1f12446037e0b22b99cd9-full.png",
-  "01n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/b70152928f0f1353f1b9678b11e4e143-full.png",
-  "02d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/e352028844abdee3c13dbad2539ac9bd-full.png",
-  "02n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/8202e39260f245b90f0675002f3ab0ed-full.png",
-  "03d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/a643cca92e4d37c33ba087b740e1ac68-full.png",
-  "03n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/dfc0e48a0db6301efb2981fb6af7dc82-full.png",
-  "04d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/163e8a5f8a631d5afa9c5c9fe59561d9-full.png",
-  "04n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/c953de8cbf53fb9e22a11f34788b8186-full.png",
-  "09d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/11e1558eea5e773bbc4a1847d34e7949-full.png",
-  "09n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/c7ad021b16230e51e47530077dbb987b-full.png",
-  "10d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/b02235ab434638777a465a4619c7440e-full.png",
-  "10n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/b02235ab434638777a465a4619c7440e-full.png",
-  "11d":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/516f19fd49a0ff1bc6656c6b613a3353-full.png",
-  "11n":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/6fdadc7ac191e9b0c7854b88a672a340-full.png",
-  "50d": "http://openweathermap.org/img/wn/50n@2x.png",
-  "50n": "http://openweathermap.org/img/wn/50d@2x.png",
-  "01x":
-    "https://cdn1.bbcode0.com/uploads/2021/10/17/1a92947adb3e5d3d455862c4038af627-full.png",
+  "01d":c01d,
+  "01n":c01n,
+  "02d":c02d,
+  "02n":c02n,
+  "03d":c03d,
+  "03n":c03n,
+  "04d":c04d,
+  "04n":c04n,
+  "09d":c09d,
+  "09n":c09n,
+  "10d":c10d,
+  "10n":c10n,
+  "11d":c11d,
+  "11n":c11n,
+  "50d":c50d,
+  "50n":c50n,
+  "01x":unknown
 };
 const Weather = ({ sunRiseStatus, weatherData, mainData, city, logo }) => {
   const date = new Date();
@@ -85,6 +88,7 @@ const Weather = ({ sunRiseStatus, weatherData, mainData, city, logo }) => {
         }
         className="video"
         autoplay="true"
+        loop="true"
         muted
       ></video>
       {city ? (
